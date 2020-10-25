e72c97c04f446b549dcb745d27a58514bf15aa52
Mathura <mathuradas333@gmail.com>
2020-10-25 21:27:06 +0530

    [Mathura]Test. Tested Deleted_A_Node_Should_Throw_LinkedListException_When_Data_Is_Not_Present

--- LinkedListMSTest/UnitTest.cs
+++ LinkedListMSTest/UnitTest.cs
@@ -136,5 +136,21 @@ namespace LinkedListMSTest
                 i++;
             }
         }
+        [TestMethod]
+        public void Deleted_A_Node_Should_Throw_LinkedListException_When_Data_Is_Not_Present()
+        {
+            try
+            {
+                LinkedListClass testingObj = new LinkedListClass();
+                testingObj.Add(90);
+                testingObj.Add(34);
+                testingObj.Add(89);
+                testingObj.Delete(35);
+            }
+            catch(Exception e)
+            {
+                Assert.AreEqual(e.Message, "35 not found in Linked List");
+            }
+        }
     }
 }
